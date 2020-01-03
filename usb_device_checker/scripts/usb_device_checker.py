8f88b434533217ecfb2a467777ed231f45b6bf15
calm0815 <calm0815@example.com>
2020-01-04 02:01:57 +0900

    gpioベースの検出方法に変更（要変更）

--- usb_device_checker/scripts/usb_device_checker.py
+++ usb_device_checker/scripts/usb_device_checker.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python
 
 import rospy
+import RPi.GPIO as GPIO
 from std_msgs.msg import Bool
-import pyudev
 
-context = pyudev.Context()
-monitor = pyudev.Monitor.from_netlink(context)
-monitor.filter_by(subsystem='usb')
-monitor.start()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
 
-status = False
-
-def checker():
-  for device in iter(monitor.poll, None):
-    if device.action == 'add' or device.action == 'bind':
-      status = True
-      print('USB device status : connected')
-    elif device.action == 'remove' or device.action == 'unbind':
-      status = False
-      print('USB device status : disconnected')
-
-def status_pub():
+def status_publisher():
   pub = rospy.Publisher('usb_status', Bool, queue_size=10)
   rospy.init_node('usb_device_checker', anonymous=True)
   r = rospy.Rate(10) # 10hz
   while not rospy.is_shutdown():
-    checker()
+    if GPIO.input(12) == GPIO.LOW:
+      status = False
+    else:
+      status = True
     rospy.loginfo(status)
     pub.publish(status)
     r.sleep()
 
 if __name__ == '__main__':
   try:
-    status_pub()
+    status_publisher()
   except rospy.ROSInterruptException: pass
